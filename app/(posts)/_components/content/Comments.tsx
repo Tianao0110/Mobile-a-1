064bf4d602c1ed16821a5b14073632910c2e7c63
Tianao0110 <123719125+Tianao0110@users.noreply.github.com>
2026-02-05 14:40:08 -0700

    Comments

--- app/(posts)/_components/content/Comments.tsx
+++ app/(posts)/_components/content/Comments.tsx
@@ -1,12 +1,70 @@
+import Avatar from "@/app/components/Avatar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 const Comments = () => {
-  return <View style={styles.container}>Comments area</View>;
+  return (
+    <view style={styles.container}>
+      <view style={{flexDirection:"row"}}>
+        <view style={{flexDirection:"row"}}>
+          <Avatar
+          uri ="https://i.pravatar.cc/150?img=3"
+          width = {20}
+          style = {{zIndex:3}}>
+          </Avatar>
+          <Avatar
+          uri = "https://i.pravatar.cc/150?img=4"
+          width = {20}
+          style = {{zIndex: 1, marginLeft: -4}}>
+          </Avatar>
+          <Avatar 
+          uri = "https://i.pravatar.cc/150?img=5"
+          width={20}
+          style= {{zIndex:1, marginLeft: -4, marginRight:10}}>
+          </Avatar>
+        </view>
+        <Text style = {styles.commonFont}>
+          <Text>Liked by</Text>
+          <Text style = {{fontWeight: 700}}>paisley.print.48</Text>
+          <Text>and</Text>
+          <Text style = {{fontWeight: 700}}>7 others</Text>
+        </Text>
+      </view>
+      <Text>
+
+        <Text style = {[styles.commonFont, { fontWeight: 700}]}>frenchie_fry39{" "}</Text>
+
+        <Text style = {styles.commonFont}>Fresh shot on a sunny! ☀</Text>
+
+        <Text style = {{color:"#8d8d8d"}}>View all 12 comments</Text>
+      </Text>
+
+      <Text>
+        <Text style = {{fontWeight: 700}}>lil_wyatt838</Text>
+        <Text>Awesome tones</Text>
+      </Text>
+
+      <Text>
+        <Text style = {{fontWeight: 700}}>pia.in.a.pod</Text>
+        <Text>Gorg. Love it! ❤</Text>
+      </Text>
+
+      <Text style = {{color: "#8d8d8d"}}>1 day ago        
+      </Text>
+    </view>
+  );
 };
 
 export default Comments;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    paddingLeft:10,
+    paddingRight:10,
+    gap:8,
+  },
+
+  commonFont: {
+    fontSize:16,
+  },
 });
